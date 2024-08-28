24b0467b0b166a2150f1b794c32fbc91a7def082
KeeganBeuthin <kdbeuthin@gmail.com>
2024-08-28 12:37:37 +0200

    added readHtlmFile

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -5,6 +5,7 @@ package utils
 
 import (
 	"fmt"
+	"os"
 	"unsafe"
 )
 
@@ -54,6 +55,15 @@ func malloc(size int32) unsafe.Pointer {
 	}
 	ptr := wasmMalloc(uint32(size))
 	return unsafe.Pointer(uintptr(ptr))
+}
+
+// ReadHtmlFile reads the content of an HTML file and returns it as a string
+func ReadHtmlFile(filePath string) (string, error) {
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", fmt.Errorf("error reading HTML file: %v", err)
+	}
+	return string(content), nil
 }
 
 // Free is a placeholder for freeing memory (if needed)
