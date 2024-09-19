b1260e4104610dbd2b2b4bb79178fa51f414d77b
KeeganBeuthin <kdbeuthin@gmail.com>
2024-09-19 23:53:52 +0200

    changing to fomratting again

--- pkg/transactions/transactions.go
+++ pkg/transactions/transactions.go
@@ -1,9 +1,12 @@
+//go:build js && wasm
+// +build js,wasm
+
 package transactions
 
 import (
+	"encoding/json"
 	"fmt"
 	"strconv"
-	"strings"
 
 	"github.com/KeeganBeuthin/TBV-Go-SDK/pkg/utils"
 )
@@ -13,52 +16,53 @@ var globalAmount float64
 func Execute_credit_leg(amountPtr *byte, amountLen int32, accountPtr *byte, accountLen int32) *byte {
 	amount := utils.GoString(amountPtr, amountLen)
 	account := utils.GoString(accountPtr, accountLen)
+
 	amountFloat, err := strconv.ParseFloat(amount, 64)
 	if err != nil {
-		return utils.CreateErrorResult(fmt.Sprintf("Error: Invalid amount value: %s", amount))
+		return utils.CreateErrorResult(fmt.Sprintf("Invalid amount value: %s", amount))
 	}
 	globalAmount = amountFloat
+
 	fmt.Printf("Executing credit leg for amount: %s, account: %s\n", amount, account)
-	query := fmt.Sprintf(`
-		PREFIX ex: <http://example.org/>
-		SELECT ?balance
-		WHERE {
-			ex:%s ex:hasBalance ?balance .
-		}
-	`, account)
+
+	query := fmt.Sprintf(`PREFIX ex: <http://example.org/>
+        SELECT ?balance
+        WHERE {
+          ex:%s ex:hasBalance ?balance .
+        }`, account)
+
 	return utils.StringToPtr(query)
 }
 
 func Process_credit_result(resultPtr *byte) *byte {
-	if resultPtr == nil {
-		return utils.CreateErrorResult("Error: Received nil pointer")
-	}
 	result := utils.GoString(resultPtr, -1)
-	fmt.Printf("Processing credit result: %s\n", result)
+	fmt.Printf("Processing result: %s, amount: %.2f\n", result, globalAmount)
 
-	// Find the balance value in the JSON string
-	balanceStart := strings.Index(result, `"balance":"`) + 11
-	balanceEnd := strings.Index(result[balanceStart:], `"`)
-
-	if balanceStart > 10 && balanceEnd > 0 {
-		balanceStr := result[balanceStart : balanceStart+balanceEnd]
-		balance, err := strconv.ParseFloat(balanceStr, 64)
-
-		if err == nil {
-			newBalance := balance + globalAmount
-			responseMessage := fmt.Sprintf("Current balance: %.2f. After credit of %.2f, new balance: %.2f", balance, globalAmount, newBalance)
-			fmt.Println(responseMessage)
-			return utils.StringToPtr(responseMessage)
-		} else {
-			errorMessage := fmt.Sprintf("Invalid balance value: %s", balanceStr)
-			fmt.Println(errorMessage)
-			return utils.CreateErrorResult(errorMessage)
-		}
-	} else {
-		errorMessage := "No balance found in result"
-		fmt.Println(errorMessage)
-		return utils.CreateErrorResult(errorMessage)
+	var data struct {
+		Results []struct {
+			Balance string `json:"balance"`
+		} `json:"results"`
 	}
+
+	err := json.Unmarshal([]byte(result), &data)
+	if err != nil {
+		return utils.CreateErrorResult(fmt.Sprintf("Failed to unmarshal result: %v", err))
+	}
+
+	if len(data.Results) == 0 {
+		return utils.CreateErrorResult("No balance found in result")
+	}
+
+	balance, err := strconv.ParseFloat(data.Results[0].Balance, 64)
+	if err != nil {
+		return utils.CreateErrorResult(fmt.Sprintf("Invalid balance value: %s", data.Results[0].Balance))
+	}
+
+	newBalance := balance + globalAmount
+	responseMessage := fmt.Sprintf("Current balance: %.0f. After credit of %.0f, new balance: %.0f", balance, globalAmount, newBalance)
+	fmt.Println(responseMessage)
+
+	return utils.StringToPtr(responseMessage)
 }
 
 func Execute_debit_leg(amountPtr *byte, amountLen int32, accountPtr *byte, accountLen int32) *byte {
